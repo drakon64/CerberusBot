b9cd9912e8d1c08eba7179ee9a9c228881a6e904
Adam Chance <6444703+drakon64@users.noreply.github.com>
2022-11-21 14:11:18 +0000

    Import `io.ktor.server.cio.EngineMain`

--- discord/selfcontained/src/commonMain/kotlin/Application.kt
+++ discord/selfcontained/src/commonMain/kotlin/Application.kt
@@ -3,8 +3,9 @@ package cloud.drakon.tempestbot
 import cloud.drakon.tempestbot.plugins.configureRouting
 import cloud.drakon.tempestbot.plugins.configureSerialization
 import io.ktor.server.application.Application
+import io.ktor.server.cio.EngineMain
 
-fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
     configureRouting()
