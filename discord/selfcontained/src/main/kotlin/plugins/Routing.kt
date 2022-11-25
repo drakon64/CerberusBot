aa48f4807d6b58cebba99e4de6080010bb26a82f
Adam Chance <6444703+drakon64@users.noreply.github.com>
2022-11-25 14:40:29 +0000

    Add `/healthcheck` route

--- discord/selfcontained/src/main/kotlin/plugins/Routing.kt
+++ discord/selfcontained/src/main/kotlin/plugins/Routing.kt
@@ -1,13 +1,24 @@
 package cloud.drakon.tempestbot.plugins
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.application.install
+import io.ktor.server.routing.get
 import io.ktor.server.routing.post
+import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
     routing {
-        post("/") {
+        route("/") {
+            this@configureRouting.install(DiscordInteractionsPlugin)
+            post {
 
+            }
+        }
+        get("/healthcheck") {
+            call.response.status(HttpStatusCode.OK)
         }
     }
 }
