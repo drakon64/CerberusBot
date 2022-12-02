0f189864a40d7ee7062711c458cb5aa4fc900af9
Adam Chance <6444703+drakon64@users.noreply.github.com>
2022-12-02 16:55:38 +0000

    Add the XivApi client class

---
+++ sam/interact/src/main/kotlin/api/XivApiClient.kt
@@ -0,0 +1,75 @@
+package cloud.drakon.tempestbot.interact.api
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.java.Java
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+
+class XivApiClient(private val ktorClient: HttpClient = HttpClient(Java)) {
+    private fun Boolean.toBinaryString(): String {
+        return if (this) {
+            "1"
+        } else {
+            "0"
+        }
+    }
+
+    /**
+     * @param indexes Search a specific series of indexes separated by commas.
+     * @param string The string to search for. The results for this are affected by `string_column` and `string_algo`.
+     * @param stringAlgo The search algorithm to use for string matching. **Default:** wildcard
+     */
+    suspend fun search(
+        string: String,
+        indexes: String? = null,
+        stringAlgo: String? = null,
+        language: String? = null,
+        snakeCase: Boolean? = null,
+        privateKey: String? = null,
+    ): String {
+        return ktorClient.get("https://xivapi.com/search") {
+            url {
+                parameters.append("string", string)
+
+                // Global parameters
+                if (language != null) {
+                    parameters.append("language", language)
+                }
+                if (snakeCase != null) {
+                    parameters.append("snake_case", snakeCase.toBinaryString())
+                }
+                if (privateKey != null) {
+                    parameters.append("private_key", privateKey)
+                }
+
+                if (indexes != null) {
+                    parameters.append("indexes", indexes)
+                }
+                if (stringAlgo != null) {
+                    parameters.append("string_algo", stringAlgo)
+                }
+            }
+        }.bodyAsText()
+    }
+
+    suspend fun item(
+        item: Int,
+        language: String? = null,
+        snakeCase: Boolean? = null,
+        privateKey: String? = null,
+    ): String {
+        return ktorClient.get("https://xivapi.com/item/$item") {
+            url { // Global parameters
+                if (language != null) {
+                    parameters.append("language", language)
+                }
+                if (snakeCase != null) {
+                    parameters.append("snake_case", snakeCase.toBinaryString())
+                }
+                if (privateKey != null) {
+                    parameters.append("private_key", privateKey)
+                }
+            }
+        }.bodyAsText()
+    }
+}
