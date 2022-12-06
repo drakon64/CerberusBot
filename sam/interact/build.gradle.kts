9527ff111d8504916f39cf1e106eebd990073bd9
Adam Chance <6444703+drakon64@users.noreply.github.com>
2022-12-06 21:09:15 +0000

    Remove `aws-lambda-java-events` from `interact`

--- sam/interact/build.gradle.kts
+++ sam/interact/build.gradle.kts
@@ -17,7 +17,6 @@ dependencies {
     implementation("cloud.drakon:tempest:0.0.1-SNAPSHOT")
 
     implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
-    implementation("com.amazonaws:aws-lambda-java-events:3.11.0")
 
     // Translate
     implementation("aws.sdk.kotlin:translate:0.18.0-beta")
