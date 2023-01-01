330c5add8c0a6d3cdd82f573eb4d38d33bca9080
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-01-01 20:29:06 +0000

    Bump `KtDiscord` to `2.0.0`

--- sam/defer/build.gradle.kts
+++ sam/defer/build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 }
 
 dependencies {
-    implementation("cloud.drakon:ktdiscord:1.0.3")
+    implementation("cloud.drakon:ktdiscord:2.0.0")
 
     implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
     implementation("com.amazonaws:aws-lambda-java-events:3.11.0")
--- sam/interact/build.gradle.kts
+++ sam/interact/build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 }
 
 dependencies {
-    implementation("cloud.drakon:ktdiscord:1.0.3")
+    implementation("cloud.drakon:ktdiscord:2.0.0")
 
     implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
 
