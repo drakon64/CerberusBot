e8cb5513e9b207150ec5c9bcbc87148eec9e715b
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-01-07 17:17:08 +0000

    Upload attachments as files

--- sam/interact/src/main/kotlin/commands/citations/addCitation.kt
+++ sam/interact/src/main/kotlin/commands/citations/addCitation.kt
@@ -1,6 +1,7 @@
 package cloud.drakon.tempestbot.interact.commands.citations
 
 import cloud.drakon.ktdiscord.channel.Attachment
+import cloud.drakon.ktdiscord.file.File
 import cloud.drakon.ktdiscord.interaction.Interaction
 import cloud.drakon.ktdiscord.interaction.applicationcommand.ApplicationCommandData
 import cloud.drakon.ktdiscord.webhook.EditWebhookMessage
@@ -8,6 +9,10 @@ import cloud.drakon.tempestbot.interact.Handler
 import cloud.drakon.tempestbot.interact.Handler.Companion.json
 import com.mongodb.client.model.Filters
 import com.mongodb.client.model.Updates
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.java.Java
+import io.ktor.client.request.get
 import kotlinx.coroutines.delay
 import kotlinx.serialization.encodeToString
 import org.bson.BsonArray
@@ -72,17 +77,50 @@ suspend fun addCitation(event: Interaction<ApplicationCommandData>) {
     )
 
     if (query != null && query.isNotEmpty()) {
+        val files = ArrayList<File>()
+        val webhookAttachments = ArrayList<Attachment>()
+        if (attachments != null) {
+            for (i in attachments) {
+                files.add(
+                    File(
+                        i.id,
+                        i.filename,
+                        i.contentType !!,
+                        HttpClient(Java).get(i.url !!).body() as ByteArray
+                    )
+                )
+                webhookAttachments.add(Attachment(i.id, i.filename, i.contentType))
+            }
+        }
         if (message.isNotEmpty()) {
             Handler.ktDiscordClient.editOriginalInteractionResponse(
                 EditWebhookMessage(
                     content = "> " + message.replace("\n", "\n> ") + "\n- <@$userId>",
-                    attachments = attachments
+                    files = if (files.isNotEmpty()) {
+                        files.toTypedArray()
+                    } else {
+                        null
+                    },
+                    attachments = if (webhookAttachments.isNotEmpty()) {
+                        webhookAttachments.toTypedArray()
+                    } else {
+                        null
+                    }
                 ), interactionToken = event.token
             )
         } else {
             Handler.ktDiscordClient.editOriginalInteractionResponse(
-                EditWebhookMessage(attachments = attachments),
-                interactionToken = event.token
+                EditWebhookMessage(
+                    files = if (files.isNotEmpty()) {
+                        files.toTypedArray()
+                    } else {
+                        null
+                    }, attachments = if (webhookAttachments.isNotEmpty()) {
+                        webhookAttachments.toTypedArray()
+                    } else {
+                        null
+                    }
+                ), interactionToken = event.token
             )
         }
     } else {
