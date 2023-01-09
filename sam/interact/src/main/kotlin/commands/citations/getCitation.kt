b46098ccc9ef344df1dc2500adaf1a5bb8227548
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-01-09 10:44:38 +0000

    Revert "Revert getting attachments from citations"
    This reverts commit 1ff07ce23f0c7b68c7381d47561b67bf2dab99bc.

--- sam/interact/src/main/kotlin/commands/citations/getCitation.kt
+++ sam/interact/src/main/kotlin/commands/citations/getCitation.kt
@@ -1,12 +1,19 @@
 package cloud.drakon.tempestbot.interact.commands.citations
 
+import cloud.drakon.ktdiscord.channel.Attachment
+import cloud.drakon.ktdiscord.file.File
 import cloud.drakon.ktdiscord.interaction.Interaction
 import cloud.drakon.ktdiscord.interaction.applicationcommand.ApplicationCommandData
 import cloud.drakon.ktdiscord.webhook.EditWebhookMessage
 import cloud.drakon.tempestbot.interact.Handler
 import com.mongodb.client.model.Filters
 import com.mongodb.client.model.Projections
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.java.Java
+import io.ktor.client.request.get
 import kotlinx.coroutines.delay
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
@@ -33,20 +40,38 @@ suspend fun getCitation(event: Interaction<ApplicationCommandData>) {
     ).first()
 
     var error = true
-    lateinit var content: String
+    val content: String
+    val attachments = ArrayList<Attachment>()
 
     if (citations != null) {
         val messagesJson =
             Handler.json.parseToJsonElement(citations.toJson()).jsonObject["messages"] !!.jsonArray
-        val messages: MutableList<String> = emptyList<String>().toMutableList()
+        val messages = ArrayList<String>()
 
         if (messagesJson.isNotEmpty()) {
             for (i in messagesJson) {
-                messages.add(i.jsonObject["content"] !!.jsonPrimitive.content)
+                if (i.jsonObject["content"] !!.jsonPrimitive.content.isNotEmpty() && i.jsonObject["content"] !!.jsonPrimitive.content != "null") { // TODO: Serialise to a class so we don't have this ugly null check
+                    messages.add(
+                        i.jsonObject["content"] !!.jsonPrimitive.content
+                    )
+                }
+                if (i.jsonObject["attachments"] !!.jsonArray.isNotEmpty()) {
+                    for (j in i.jsonObject["attachments"] !!.jsonArray) {
+                        attachments.add(
+                            Json.decodeFromJsonElement(
+                                Attachment.serializer(), j.jsonObject
+                            )
+                        )
+                    }
+                }
             }
 
             error = false
-            content = "> " + messages.random().replace("\n", "\n> ") + "\n- <@$userId>"
+            content = if (messages.isNotEmpty()) {
+                "> " + messages.random().replace("\n", "\n> ") + "\n- <@$userId>"
+            } else {
+                "- <@$userId>"
+            }
         } else {
             content = "No citations saved for the user!"
         }
@@ -54,9 +79,29 @@ suspend fun getCitation(event: Interaction<ApplicationCommandData>) {
         content = "User has not opted-in to citations!"
     }
 
+    val files = ArrayList<File>()
+    for (i in attachments) {
+        files.add(
+            File(
+                i.id,
+                i.filename,
+                i.contentType !!,
+                HttpClient(Java).get(i.url !!).body() as ByteArray
+            )
+        )
+    }
+
     Handler.ktDiscordClient.editOriginalInteractionResponse(
         EditWebhookMessage(
-            content = content
+            content = content, files = if (files.isNotEmpty()) {
+                files.toTypedArray()
+            } else {
+                null
+            }, attachments = if (attachments.isNotEmpty()) {
+                attachments.toTypedArray()
+            } else {
+                null
+            }
         ), interactionToken = event.token
     )
 
