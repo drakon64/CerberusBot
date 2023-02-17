b5d9c8a3806de76f06bda9decde8f2a9ecb1eda1
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-02-17 22:03:23 +0000

    Fix incorrect citation count

--- sam/interact/src/main/kotlin/commands/citations/stats.kt
+++ sam/interact/src/main/kotlin/commands/citations/stats.kt
@@ -8,6 +8,7 @@ import cloud.drakon.ktdiscord.webhook.EditWebhookMessage
 import cloud.drakon.tempestbot.interact.Handler
 import com.mongodb.client.model.Filters
 import com.mongodb.client.model.Projections
+import kotlinx.coroutines.delay
 
 suspend fun stats(event: Interaction<ApplicationCommandData>) {
     lateinit var userId: String
@@ -22,22 +23,51 @@ suspend fun stats(event: Interaction<ApplicationCommandData>) {
         2 -> userId = event.data !!.resolved !!.users !!.keys.first()
     }
 
-    val citationsCount = mongoCollection.find(
+    val citations = mongoCollection.find(
         Filters.and(
             Filters.eq("user_id", userId), Filters.eq("guild_id", event.guildId)
         )
     ).projection(
         Projections.fields(Projections.include("messages"), Projections.excludeId())
-    ).count().toString()
+    ).first()
+
+    var error = true
+    lateinit var citationCount: String
+    lateinit var errorString: String
+
+    if (citations != null) {
+        val messagesJson: Citations =
+            Handler.json.decodeFromString(Citations.serializer(), citations.toJson())
+
+        if (messagesJson.messages != null) {
+            error = false
+
+            citationCount = messagesJson.messages.count().toString()
+        } else {
+            errorString = "No citations saved for the user!"
+        }
+    } else {
+        errorString = "User has not opted-in to citations!"
+    }
 
     Handler.ktDiscordClient.editOriginalInteractionResponse(
-        EditWebhookMessage(
-            embeds = arrayOf(
-                Embed(
-                    "Citation stats for user",
-                    fields = arrayOf(EmbedField("Count", citationsCount))
+        if (! error) {
+            EditWebhookMessage(
+                embeds = arrayOf(
+                    Embed(
+                        "Citation stats for user",
+                        fields = arrayOf(EmbedField("Count", citationCount))
+                    )
                 )
             )
-        ), interactionToken = event.token
+        } else {
+            EditWebhookMessage(errorString)
+        }, interactionToken = event.token
     )
+
+    if (error) {
+        delay(5000)
+
+        Handler.ktDiscordClient.deleteOriginalInteractionResponse(event.token)
+    }
 }
