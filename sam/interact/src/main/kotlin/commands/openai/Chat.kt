9fa00d3d7021b94c4ad2f36a70cc42f5e6044741
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-04-05 20:09:34 +0100

    Set GPT temperature to max

--- sam/interact/src/main/kotlin/commands/openai/Chat.kt
+++ sam/interact/src/main/kotlin/commands/openai/Chat.kt
@@ -61,7 +61,7 @@ suspend fun chat(event: Interaction<ApplicationCommandData>) = coroutineScope {
         ChatRequest(
             "gpt-3.5-turbo",
             messages.toTypedArray(),
-            temperature = 0.2,
+            temperature = 2.0,
             maxTokens = 2000
         )
     ).choices[0].message
