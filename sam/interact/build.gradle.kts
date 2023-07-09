0eacce1884d49cb509a24b8e22b092fc323476d2
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-07-09 21:34:28 +0100

    Bump to Java 17

--- sam/interact/build.gradle.kts
+++ sam/interact/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
 
 tasks {
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "17"
     }
     test {
         useJUnitPlatform()
