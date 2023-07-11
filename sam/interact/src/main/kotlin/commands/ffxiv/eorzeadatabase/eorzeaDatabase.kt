3024f5d379b0ae92d9f25bed1eda9e18e379b770
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-07-11 19:41:14 +0100

    Allow searching in multiple languages

--- sam/interact/src/main/kotlin/commands/ffxiv/eorzeadatabase/eorzeaDatabase.kt
+++ sam/interact/src/main/kotlin/commands/ffxiv/eorzeadatabase/eorzeaDatabase.kt
@@ -4,6 +4,7 @@ import cloud.drakon.ktdiscord.interaction.Interaction
 import cloud.drakon.ktdiscord.interaction.applicationcommand.ApplicationCommandData
 import cloud.drakon.ktdiscord.webhook.EditWebhookMessage
 import cloud.drakon.ktxivapi.KtXivApi
+import cloud.drakon.ktxivapi.common.Language
 import cloud.drakon.ktxivapi.search.StringAlgo
 import cloud.drakon.tempestbot.interact.Handler.Companion.ktDiscord
 import com.amazonaws.services.lambda.runtime.LambdaLogger
@@ -23,21 +24,39 @@ suspend fun eorzeaDatabase(
 
     lateinit var index: String
     lateinit var string: String
+    var language: String? = null
 
     for (i in event.data !!.options !!) {
         when (i.name) {
             "index" -> index = i.value !!
             "string" -> string = i.value !!
+            "language" -> language = i.value !!
         }
     }
 
+    val searchLanguage = if (language != null) {
+        when (language) {
+            "en" -> Language.en
+            "ja" -> Language.ja
+            "de" -> Language.de
+            "fr" -> Language.fr
+            else -> throw Throwable("Unknown language: \"$language\"")
+        }
+    } else {
+        null
+    }
+
     val search = KtXivApi.search(
-        string, indexes = listOf(index), stringAlgo = StringAlgo.fuzzy, limit = 1
+        string,
+        indexes = listOf(index),
+        stringAlgo = StringAlgo.fuzzy,
+        limit = 1,
+        language = searchLanguage
     ).jsonObject["Results"] !!.jsonArray.getOrNull(0)
 
     if (search != null) {
         val id = search.jsonObject["ID"] !!.jsonPrimitive.int
-        val item = KtXivApi.getContentId(index, id)
+        val item = KtXivApi.getContentId(index, id, searchLanguage)
 
         val embed = when (item["ItemKind"] !!.jsonObject["ID"] !!.jsonPrimitive.int) {
             1 -> { // Arms
