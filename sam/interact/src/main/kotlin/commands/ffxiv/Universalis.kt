16c4e35dd6d4363673d8ebe5f93050a9ec6ccde6
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-07-12 21:59:27 +0100

    Fix Universalis error if item could not be found

--- sam/interact/src/main/kotlin/commands/ffxiv/Universalis.kt
+++ sam/interact/src/main/kotlin/commands/ffxiv/Universalis.kt
@@ -47,138 +47,152 @@ suspend fun universalis(
         item,
         indexes = "Item",
         columns = arrayOf("CanBeHq", "Description", "IconHD", "ID", "Name")
-    ).jsonObject["Results"] !!.jsonArray[0]
-    val xivApiItemId = xivApiItem.jsonObject["ID"] !!.jsonPrimitive.int
+    ).jsonObject["Results"]?.jsonArray?.get(0)
 
-    val canBeHighQuality: Boolean =
-        xivApiItem.jsonObject["CanBeHq"] !!.jsonPrimitive.int == 1
-    val description: String = Jsoup.clean(
-        xivApiItem.jsonObject["Description"] !!.jsonPrimitive.content,
-        "",
-        Safelist.none(),
-        Document.OutputSettings().prettyPrint(false)
-    ).replace("""\n{3,}""".toRegex(), "\n\n")
+    if (xivApiItem != null) {
+        val xivApiItemId = xivApiItem.jsonObject["ID"] !!.jsonPrimitive.int
 
-    val marketBoardCurrentData = if (highQuality == true && canBeHighQuality) {
-        KtUniversalisClient.getMarketBoardCurrentData(
-            world,
-            arrayOf(xivApiItemId).toIntArray(),
-            entries = 5,
-            listings = 5,
-            hq = true
-        )
-    } else if (highQuality == false) {
-        KtUniversalisClient.getMarketBoardCurrentData(
-            world,
-            arrayOf(xivApiItemId).toIntArray(),
-            entries = 5,
-            listings = 5,
-            hq = false
-        )
-    } else {
-        KtUniversalisClient.getMarketBoardCurrentData(
-            world, arrayOf(xivApiItemId).toIntArray(), entries = 5, listings = 5
-        )
-    }
-    val marketBoardListings = marketBoardCurrentData.listings
-    var listings = ""
-    var totalPrices = ""
-    val gil = "<:gil:235457032616935424>"
+        val canBeHighQuality: Boolean =
+            xivApiItem.jsonObject["CanBeHq"] !!.jsonPrimitive.int == 1
+        val description: String = Jsoup.clean(
+            xivApiItem.jsonObject["Description"] !!.jsonPrimitive.content,
+            "",
+            Safelist.none(),
+            Document.OutputSettings().prettyPrint(false)
+        ).replace("""\n{3,}""".toRegex(), "\n\n")
 
-    if (! marketBoardListings.isNullOrEmpty()) {
-        for (i in marketBoardListings) {
-            listings += String.format(
-                "%,d", i.pricePerUnit
-            ) + " $gil x " + i.quantity + " [" + i.worldName + "]" + if (i.hq) {
-                " <:hq:916051971063054406>\n"
-            } else {
-                "\n"
-            }
-            totalPrices += String.format(
-                "%,d", i.total
-            ) + " $gil\n"
+        val marketBoardCurrentData = if (highQuality == true && canBeHighQuality) {
+            KtUniversalisClient.getMarketBoardCurrentData(
+                world,
+                arrayOf(xivApiItemId).toIntArray(),
+                entries = 5,
+                listings = 5,
+                hq = true
+            )
+        } else if (highQuality == false) {
+            KtUniversalisClient.getMarketBoardCurrentData(
+                world,
+                arrayOf(xivApiItemId).toIntArray(),
+                entries = 5,
+                listings = 5,
+                hq = false
+            )
+        } else {
+            KtUniversalisClient.getMarketBoardCurrentData(
+                world, arrayOf(xivApiItemId).toIntArray(), entries = 5, listings = 5
+            )
         }
-    } else {
-        listings = "None"
-        totalPrices = "N/A"
-    }
+        val marketBoardListings = marketBoardCurrentData.listings
+        var listings = ""
+        var totalPrices = ""
+        val gil = "<:gil:235457032616935424>"
 
-    ktDiscord.editOriginalInteractionResponse(
-        EditWebhookMessage(
-            embeds = arrayOf(
-                Embed(
-                    title = "Current prices for " + xivApiItem.jsonObject["Name"] !!.jsonPrimitive.content,
-                    description = description,
-                    url = "https://universalis.app/market/$xivApiItemId",
-                    thumbnail = EmbedThumbnail("https://xivapi.com" + xivApiItem.jsonObject["IconHD"] !!.jsonPrimitive.content),
-                    fields = arrayOf(
-                        if (highQuality == true && canBeHighQuality && (marketBoardCurrentData.currentAveragePriceHq > 0)) {
+        if (! marketBoardListings.isNullOrEmpty()) {
+            for (i in marketBoardListings) {
+                listings += String.format(
+                    "%,d", i.pricePerUnit
+                ) + " $gil x " + i.quantity + " [" + i.worldName + "]" + if (i.hq) {
+                    " <:hq:916051971063054406>\n"
+                } else {
+                    "\n"
+                }
+                totalPrices += String.format(
+                    "%,d", i.total
+                ) + " $gil\n"
+            }
+        } else {
+            listings = "None"
+            totalPrices = "N/A"
+        }
+
+        ktDiscord.editOriginalInteractionResponse(
+            EditWebhookMessage(
+                embeds = arrayOf(
+                    Embed(
+                        title = "Current prices for " + xivApiItem.jsonObject["Name"] !!.jsonPrimitive.content,
+                        description = description,
+                        url = "https://universalis.app/market/$xivApiItemId",
+                        thumbnail = EmbedThumbnail("https://xivapi.com" + xivApiItem.jsonObject["IconHD"] !!.jsonPrimitive.content),
+                        fields = arrayOf(
+                            if (highQuality == true && canBeHighQuality && (marketBoardCurrentData.currentAveragePriceHq > 0)) {
+                                EmbedField(
+                                    name = "Current average price (HQ)",
+                                    value = String.format(
+                                        "%,f",
+                                        marketBoardCurrentData.currentAveragePriceHq
+                                    ).trimEnd('0') + " $gil",
+                                    inline = false
+                                )
+                            } else if (highQuality == false && (marketBoardCurrentData.currentAveragePriceNq > 0)) {
+                                EmbedField(
+                                    name = "Current average price (NQ)",
+                                    value = String.format(
+                                        "%,f",
+                                        marketBoardCurrentData.currentAveragePriceNq
+                                    ).trimEnd('0') + " $gil",
+                                    inline = false
+                                )
+                            } else if (highQuality == null && (marketBoardCurrentData.currentAveragePrice > 0)) {
+                                EmbedField(
+                                    name = "Current average price",
+                                    value = String.format(
+                                        "%,f",
+                                        marketBoardCurrentData.currentAveragePrice
+                                    ).trimEnd('0') + " $gil",
+                                    inline = false
+                                )
+                            } else {
+                                EmbedField(
+                                    name = "Current average price",
+                                    value = "N/A",
+                                    inline = false
+                                )
+                            },
+                            if (highQuality == true && canBeHighQuality && (marketBoardCurrentData.averagePriceHq > 0)) {
+                                EmbedField(
+                                    name = "Historic average price (HQ)",
+                                    value = String.format(
+                                        "%,f", marketBoardCurrentData.averagePriceHq
+                                    ).trimEnd('0') + " $gil",
+                                    inline = false
+                                )
+                            } else if (highQuality == false && (marketBoardCurrentData.averagePriceNq > 0)) {
+                                EmbedField(
+                                    name = "Historic average price (NQ)",
+                                    value = String.format(
+                                        "%,f", marketBoardCurrentData.averagePriceNq
+                                    ).trimEnd('0') + " $gil",
+                                    inline = false
+                                )
+                            } else if (highQuality == null && (marketBoardCurrentData.averagePrice > 0)) {
+                                EmbedField(
+                                    name = "Historic average price",
+                                    value = String.format(
+                                        "%,f", marketBoardCurrentData.averagePrice
+                                    ).trimEnd('0') + " $gil",
+                                    inline = false
+                                )
+                            } else {
+                                EmbedField(
+                                    name = "Historic average price",
+                                    value = "N/A",
+                                    inline = false
+                                )
+                            },
                             EmbedField(
-                                name = "Current average price (HQ)",
-                                value = String.format(
-                                    "%,f", marketBoardCurrentData.currentAveragePriceHq
-                                ).trimEnd('0') + " $gil",
-                                inline = false
-                            )
-                        } else if (highQuality == false && (marketBoardCurrentData.currentAveragePriceNq > 0)) {
+                                name = "Listings", value = listings, inline = true
+                            ),
                             EmbedField(
-                                name = "Current average price (NQ)",
-                                value = String.format(
-                                    "%,f", marketBoardCurrentData.currentAveragePriceNq
-                                ).trimEnd('0') + " $gil",
-                                inline = false
+                                name = "Total price", value = totalPrices, inline = true
                             )
-                        } else if (highQuality == null && (marketBoardCurrentData.currentAveragePrice > 0)) {
-                            EmbedField(
-                                name = "Current average price", value = String.format(
-                                    "%,f", marketBoardCurrentData.currentAveragePrice
-                                ).trimEnd('0') + " $gil", inline = false
-                            )
-                        } else {
-                            EmbedField(
-                                name = "Current average price",
-                                value = "N/A",
-                                inline = false
-                            )
-                        },
-                        if (highQuality == true && canBeHighQuality && (marketBoardCurrentData.averagePriceHq > 0)) {
-                            EmbedField(
-                                name = "Historic average price (HQ)",
-                                value = String.format(
-                                    "%,f", marketBoardCurrentData.averagePriceHq
-                                ).trimEnd('0') + " $gil",
-                                inline = false
-                            )
-                        } else if (highQuality == false && (marketBoardCurrentData.averagePriceNq > 0)) {
-                            EmbedField(
-                                name = "Historic average price (NQ)",
-                                value = String.format(
-                                    "%,f", marketBoardCurrentData.averagePriceNq
-                                ).trimEnd('0') + " $gil",
-                                inline = false
-                            )
-                        } else if (highQuality == null && (marketBoardCurrentData.averagePrice > 0)) {
-                            EmbedField(
-                                name = "Historic average price", value = String.format(
-                                    "%,f", marketBoardCurrentData.averagePrice
-                                ).trimEnd('0') + " $gil", inline = false
-                            )
-                        } else {
-                            EmbedField(
-                                name = "Historic average price",
-                                value = "N/A",
-                                inline = false
-                            )
-                        },
-                        EmbedField(
-                            name = "Listings", value = listings, inline = true
-                        ),
-                        EmbedField(
-                            name = "Total price", value = totalPrices, inline = true
                         )
                     )
                 )
-            )
-        ), event.token
-    )
+            ), event.token
+        )
+    } else {
+        ktDiscord.editOriginalInteractionResponse(
+            EditWebhookMessage(content = "Could not find item \"$item\""), event.token
+        )
+    }
 }
