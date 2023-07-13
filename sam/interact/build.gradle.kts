7c72bbff8591a2e6dca24099f01e4c7183557bc9
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-07-13 18:01:44 +0100

    Re-arrange dependencies

--- sam/interact/build.gradle.kts
+++ sam/interact/build.gradle.kts
@@ -41,13 +41,18 @@ repositories {
 }
 
 dependencies {
+    implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
+
     implementation("cloud.drakon:ktdiscord:6.1.0")
 
     val ktorVersion = "2.3.2"
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-java:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 
-    implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
+    implementation("org.jsoup:jsoup:1.16.1")
+
     implementation("org.mongodb:mongodb-driver-sync:4.10.1")
 
     // Lodestone
@@ -61,12 +66,6 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-
-    implementation("io.ktor:ktor-client-core:$ktorVersion")
-    implementation("io.ktor:ktor-client-java:$ktorVersion")
-    implementation("io.ktor:ktor-client-auth:$ktorVersion")
-
-    implementation("org.jsoup:jsoup:1.16.1")
 
     testImplementation(kotlin("test"))
 }
