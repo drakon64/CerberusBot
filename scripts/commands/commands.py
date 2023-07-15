a1699308d49c420f2ac294836b9c1ee807b26b57
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-07-15 11:54:58 +0100

    Only upload commands if environment variables are set

--- scripts/commands/commands.py
+++ scripts/commands/commands.py
@@ -10,8 +10,13 @@ from universalis import create_universalis_command
 
 
 async def main():
-    application_id = os.environ["APPLICATION_ID"]
-    bot_token = os.environ["BOT_TOKEN"]
+    if "APPLICATION_ID" in os.environ and "BOT_TOKEN" in os.environ:
+        put_commands = True
+
+        application_id = os.environ["APPLICATION_ID"]
+        bot_token = os.environ["BOT_TOKEN"]
+    else:
+        put_commands = False
 
     eorzea_database = asyncio.create_task(create_eorzea_database_command())
     lodestone = asyncio.create_task(create_lodestone_command())
@@ -25,22 +30,23 @@ async def main():
 
     json.dump(commands, open("commands.json", "w"), ensure_ascii=False, indent=4)
 
-    print(
-        json.dumps(
-            json.loads(
-                bytes.decode(
-                    requests.put(
-                        f"https://discord.com/api/v10/applications/{application_id}/commands",
-                        headers={
-                            "Authorization": f"Bot {bot_token}",
-                        },
-                        json=commands,
-                    ).content
-                )
-            ),
-            indent=4,
+    if put_commands:
+        print(
+            json.dumps(
+                json.loads(
+                    bytes.decode(
+                        requests.put(
+                            f"https://discord.com/api/v10/applications/{application_id}/commands",
+                            headers={
+                                "Authorization": f"Bot {bot_token}",
+                            },
+                            json=commands,
+                        ).content
+                    )
+                ),
+                indent=4,
+            )
         )
-    )
 
 
 asyncio.run(main())
