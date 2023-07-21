5a2efba667cfb33b61024b435903a8af878672d0
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-07-21 23:40:50 +0100

    Formatting

--- bot/commands/eorzeadatabase/src/main/kotlin/quest/questHandler.kt
+++ bot/commands/eorzeadatabase/src/main/kotlin/quest/questHandler.kt
@@ -6,35 +6,34 @@ import cloud.drakon.ktdiscord.channel.embed.EmbedImage
 import cloud.drakon.ktdiscord.channel.embed.EmbedThumbnail
 import kotlinx.coroutines.coroutineScope
 
-suspend fun questHandler(quest: Quest, lodestone: String) =
-    coroutineScope {
-        val embedFields = mutableListOf(
-            EmbedField(
-                name = "Level",
-                value = quest.classJobLevel.toString()
-            )
+suspend fun questHandler(quest: Quest, lodestone: String) = coroutineScope {
+    val embedFields = mutableListOf(
+        EmbedField(
+            name = "Level",
+            value = quest.classJobLevel.toString()
         )
+    )
 
-        if (quest.gilReward != null) {
-            embedFields.add(
-                EmbedField(
-                    name = "Gil",
-                    value = "${quest.gilReward} <:gil:235457032616935424>"
-                )
+    if (quest.gilReward != null) {
+        embedFields.add(
+            EmbedField(
+                name = "Gil",
+                value = "${quest.gilReward} <:gil:235457032616935424>"
             )
-        }
-
-        return@coroutineScope Embed(
-            title = quest.name,
-            description = quest.journalGenre.name,
-            url = "https://$lodestone.finalfantasyxiv.com/lodestone/playguide/db/search/?q=${
-                quest.name.replace(
-                    " ",
-                    "+"
-                )
-            }&db_search_category=quest",
-            image = EmbedImage(url = "https://xivapi.com${quest.banner}"),
-            thumbnail = EmbedThumbnail(url = "https://xivapi.com${quest.journalGenre.icon}"),
-            fields = embedFields.toTypedArray()
         )
     }
+
+    return@coroutineScope Embed(
+        title = quest.name,
+        description = quest.journalGenre.name,
+        url = "https://$lodestone.finalfantasyxiv.com/lodestone/playguide/db/search/?q=${
+            quest.name.replace(
+                " ",
+                "+"
+            )
+        }&db_search_category=quest",
+        image = EmbedImage(url = "https://xivapi.com${quest.banner}"),
+        thumbnail = EmbedThumbnail(url = "https://xivapi.com${quest.journalGenre.icon}"),
+        fields = embedFields.toTypedArray()
+    )
+}
