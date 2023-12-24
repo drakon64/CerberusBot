ab7d3121d291c1cabc0d9c34e949f7f4c93ab607
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-12-24 18:36:56 +0000

    Formatting

--- defer/src/Handler.kt
+++ defer/src/Handler.kt
@@ -20,8 +20,8 @@ class Handler : RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> 
     private val publicKey: String = System.getenv("PUBLIC_KEY")
 
     override fun handleRequest(
-            input: APIGatewayV2HTTPEvent,
-            context: Context,
+        input: APIGatewayV2HTTPEvent,
+        context: Context,
     ): APIGatewayV2HTTPResponse {
         val response = APIGatewayV2HTTPResponse()
 
@@ -41,7 +41,14 @@ class Handler : RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> 
     }
 
     private fun validateRequest(headers: Map<String, String>, body: String) = Signature.getInstance("Ed25519").let {
-        it.initVerify(KeyFactory.getInstance("EdDSA").generatePublic(EdECPublicKeySpec(NamedParameterSpec("ED25519"), EdECPoint(false, BigInteger(1, publicKey.hexToByteArray())))))
+        it.initVerify(
+            KeyFactory.getInstance("EdDSA").generatePublic(
+                EdECPublicKeySpec(
+                    NamedParameterSpec("ED25519"),
+                    EdECPoint(false, BigInteger(1, publicKey.hexToByteArray()))
+                )
+            )
+        )
         it.update((headers["x-signature-timestamp"] + body).toByteArray())
         it.verify(headers["x-signature-ed25519"]!!.toByteArray())
     }
