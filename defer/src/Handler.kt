3cb2317d56320e3facf9136a44325c7b0f087d5c
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-12-24 21:01:22 +0000

    Fix validating requests

--- defer/src/Handler.kt
+++ defer/src/Handler.kt
@@ -15,9 +15,15 @@ import java.security.Signature
 import java.security.spec.EdECPoint
 import java.security.spec.EdECPublicKeySpec
 import java.security.spec.NamedParameterSpec
+import kotlin.experimental.and
 
 class Handler : RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
-    private val publicKey: String = System.getenv("PUBLIC_KEY")
+    private val publicKey = System.getenv("PUBLIC_KEY")
+        .hexToByteArray()
+        .apply {
+            this[size - 1] = this[size - 1].and(127)
+        }
+        .reversedArray()
 
     override fun handleRequest(
         input: APIGatewayV2HTTPEvent,
@@ -25,7 +31,12 @@ class Handler : RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> 
     ): APIGatewayV2HTTPResponse {
         val response = APIGatewayV2HTTPResponse()
 
-        if (validateRequest(input.headers, input.body)) {
+        if (validateRequest(
+                input.headers["x-signature-timestamp"]!!,
+                input.body,
+                input.headers["x-signature-ed25519"]!!.hexToByteArray(),
+            )
+        ) {
             response.body = Json.encodeToString(InteractionResponse(type = 1))
             response.statusCode = 200
         } else {
@@ -40,16 +51,20 @@ class Handler : RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> 
         return response
     }
 
-    private fun validateRequest(headers: Map<String, String>, body: String) = Signature.getInstance("Ed25519").let {
-        it.initVerify(
-            KeyFactory.getInstance("EdDSA").generatePublic(
-                EdECPublicKeySpec(
-                    NamedParameterSpec("ED25519"),
-                    EdECPoint(false, BigInteger(1, publicKey.hexToByteArray()))
+    // https://stackoverflow.com/questions/65780235/ed25519-in-jdk-15-parse-public-key-from-byte-array-and-verify
+    // https://github.com/myosyn/DiscordInteraKTions/blob/cd22595534f58fa742ad410545cc6c85739df958/requests-verifier/src/main/kotlin/net/perfectdreams/discordinteraktions/verifier/InteractionRequestVerifier.kt
+    private fun validateRequest(timestamp: String, body: String, signature: ByteArray) =
+        Signature.getInstance("Ed25519").apply {
+            initVerify(
+                KeyFactory.getInstance("Ed25519").generatePublic(
+                    EdECPublicKeySpec(
+                        NamedParameterSpec.ED25519,
+                        EdECPoint(
+                            publicKey[publicKey.size - 1].toInt().and(255).shr(7) == 1,
+                            BigInteger(1, publicKey))
+                    )
                 )
             )
-        )
-        it.update((headers["x-signature-timestamp"] + body).toByteArray())
-        it.verify(headers["x-signature-ed25519"]!!.toByteArray())
-    }
+            update((timestamp + body).toByteArray())
+        }.verify(signature)
 }
