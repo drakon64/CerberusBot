9d59f1e02159dec111cb3bd33a1ca5f71b62d4a2
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-12-25 00:15:42 +0000

    Formatting

--- commands/src/lib/discord/Locale.kt
+++ commands/src/lib/discord/Locale.kt
@@ -6,12 +6,27 @@ import kotlinx.serialization.Serializable
 // https://discord.com/developers/docs/reference#locales
 @Serializable
 internal enum class Locale {
-    @SerialName("ja") JAPANESE,
-    @SerialName("en-GB") ENGLISH_UK,
-    @SerialName("en-US") ENGLISH_US,
-    @SerialName("de") GERMAN,
-    @SerialName("fr") FRENCH,
-    @SerialName("zh-CN") CHINESE_CHINA,
-    @SerialName("zh-TW") CHINESE_TAIWAN,
-    @SerialName("ko") KOREAN,
+    @SerialName("ja")
+    JAPANESE,
+
+    @SerialName("en-GB")
+    ENGLISH_UK,
+
+    @SerialName("en-US")
+    ENGLISH_US,
+
+    @SerialName("de")
+    GERMAN,
+
+    @SerialName("fr")
+    FRENCH,
+
+    @SerialName("zh-CN")
+    CHINESE_CHINA,
+
+    @SerialName("zh-TW")
+    CHINESE_TAIWAN,
+
+    @SerialName("ko")
+    KOREAN,
 }
