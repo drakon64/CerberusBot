aca1240beab579b4edd0a339a087817bdb5132be
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-12-26 15:33:38 +0000

    Optimise `ephemeralCommand()`

--- commands/src/EphemeralCommand.kt
+++ commands/src/EphemeralCommand.kt
@@ -1,16 +1,25 @@
 package cloud.drakon.dynamisbot
 
 import cloud.drakon.dynamisbot.lib.discord.applicationcommand.ApplicationCommandOption
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 
-internal suspend fun ephemeralCommand(): ApplicationCommandOption {
+internal suspend fun ephemeralCommand(): ApplicationCommandOption = coroutineScope {
     val name = "Ephemeral"
     val description = "Make the bot response visible to you only"
 
-    return ApplicationCommandOption(
+    val nameLocalizations = async {
+        name.buildLocalizationMap(true)
+    }
+    val descriptionLocalizations = async {
+        description.buildLocalizationMap()
+    }
+
+    return@coroutineScope ApplicationCommandOption(
         type = 5,
         name = name.commandName(),
-        nameLocalizations = name.buildLocalizationMap(true),
+        nameLocalizations = nameLocalizations.await(),
         description = description,
-        descriptionLocalizations = description.buildLocalizationMap(),
+        descriptionLocalizations = descriptionLocalizations.await(),
     )
 }
