2bde36969be8886247e604c3201ff3365e832cca
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-12-26 19:26:54 +0000

    Minimise `defer` Jar https://github.com/johnrengelman/shadow/issues/894

--- defer/build.gradle.kts
+++ defer/build.gradle.kts
@@ -1,5 +1,11 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("com.github.johnrengelman.shadow").version("8.1.1")
+}
+
+tasks.withType<ShadowJar>().configureEach {
+    minimize()
 }
 
 tasks.withType<AbstractArchiveTask>().configureEach {
