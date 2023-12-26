5322a8356a7f4e1ac1706219746206c561681514
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-12-26 14:36:49 +0000

    Adjust localizations for `ephemeral` option

--- commands/src/EphemeralCommand.kt
+++ commands/src/EphemeralCommand.kt
@@ -3,12 +3,12 @@ package cloud.drakon.dynamisbot
 import cloud.drakon.dynamisbot.lib.discord.applicationcommand.ApplicationCommandOption
 
 internal suspend fun ephemeralCommand(): ApplicationCommandOption {
-    val name = "ephemeral"
+    val name = "Ephemeral"
     val description = "Make the bot response visible to you only"
 
     return ApplicationCommandOption(
         type = 5,
-        name = name,
+        name = name.commandName(),
         nameLocalizations = name.buildLocalizationMap(true),
         description = description,
         descriptionLocalizations = description.buildLocalizationMap(),
