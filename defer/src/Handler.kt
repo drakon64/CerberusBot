4b6cdb3aa2812f86c1f31011484dcfb107efb843
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-12-29 20:30:12 +0000

    Invoke `interact` from `defer`

--- defer/src/Handler.kt
+++ defer/src/Handler.kt
@@ -2,6 +2,9 @@
 
 package cloud.drakon.dynamisbot
 
+import aws.sdk.kotlin.services.lambda.LambdaClient
+import aws.sdk.kotlin.services.lambda.model.InvocationType
+import aws.sdk.kotlin.services.lambda.model.InvokeRequest
 import cloud.drakon.dynamisbot.discord.interaction.Interaction
 import cloud.drakon.dynamisbot.discord.interaction.InteractionResponse
 import com.amazonaws.services.lambda.runtime.Context
@@ -15,6 +18,7 @@ import java.security.spec.EdECPoint
 import java.security.spec.EdECPublicKeySpec
 import java.security.spec.NamedParameterSpec
 import kotlin.experimental.and
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
@@ -33,10 +37,14 @@ class Handler : RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> 
         isLenient = true
     }
 
+    private val lambdaClient = LambdaClient {
+        region = "eu-west-2"
+    }
+
     override fun handleRequest(
         input: APIGatewayV2HTTPEvent,
         context: Context,
-    ): APIGatewayV2HTTPResponse {
+    ): APIGatewayV2HTTPResponse = runBlocking {
         val response = APIGatewayV2HTTPResponse()
 
         // https://stackoverflow.com/questions/65780235/ed25519-in-jdk-15-parse-public-key-from-byte-array-and-verify
@@ -58,10 +66,16 @@ class Handler : RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> 
             when (json.decodeFromString<Interaction>(input.body).type) {
                 1.toByte() -> {
                     context.logger.log("Pong")
-                    response.body = Json.encodeToString(InteractionResponse(type = 1))
+                    response.body = json.encodeToString(InteractionResponse(type = 1))
                 }
 
                 else -> {
+                    lambdaClient.invoke(InvokeRequest {
+                        functionName = "DynamisBot_interact"
+                        invocationType = InvocationType.Event
+                        payload = input.body.toByteArray()
+                    })
+
                     context.logger.log("Deferring channel message")
                     response.body = Json.encodeToString(InteractionResponse(type = 5))
                 }
@@ -74,6 +88,6 @@ class Handler : RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> 
             response.statusCode = 401
         }
 
-        return response
+        return@runBlocking response
     }
 }
